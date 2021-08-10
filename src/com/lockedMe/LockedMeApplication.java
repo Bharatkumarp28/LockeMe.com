c359ec5490b4b0445d949a8c6569ca7e6387f193
Bharat Kumar P <bharatkumarp28@gmail.com>
2021-08-10 14:46:33 +0530

    Completed with Menu and Sub Menu options

--- src/com/lockedMe/LockedMeApplication.java
+++ src/com/lockedMe/LockedMeApplication.java
@@ -1,46 +1,10 @@
 package com.lockedMe;
 
-import java.util.List;
-
 public class LockedMeApplication 
 {
 
 	public static void main(String[] args) 
 	{
-		/*
-		//Retrieving the file names in an ascending order
-		List<String> filesInDirectory = LockedMeOperations.retrieveFileNames();
-		for(String file : filesInDirectory)
-			System.out.println(file);
-		
-		
-		//Addition of files and data
-		boolean isAdded = LockedMeOperations.addFile();
-		if(isAdded)
-			System.out.println("File added successfully!");
-		else
-			System.out.println("File not added");
-		
-		
-		
-		//Delete user specified file
-		boolean isDeleted = LockedMeOperations.deleteFile();
-		if(isDeleted)
-			System.out.println("File is successfully deleted!");
-		else
-			System.out.println("File does not exist in the path to delete!");
-		
-		//Display Main Menu options
-		//LockedMeMenuOptions.mainMenuOptions();
-		
-		//Search user specified file
-		boolean isFound = LockedMeOperations.searchFile();
-		if(isFound)
-			System.out.println("File found in the location");
-		else
-			System.out.println("File not found!");
-		*/
-		
 		//Display Main Menu options
 		LockedMeMenuOptions.mainMenuOptions();
 	}
